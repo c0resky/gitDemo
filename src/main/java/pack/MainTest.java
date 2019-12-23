cd81b527f257ed9c660fec9894e667e284c499b8
c0resky <pispiotr92@gmail.com>
2019-12-23 17:48:35 +0100

    Change in MainTest

--- src/main/java/pack/MainTest.java
+++ src/main/java/pack/MainTest.java
@@ -1,11 +1,26 @@
 package pack;
 
+import java.util.ArrayList;
+
 public class MainTest {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         Person p1 = new Person("Jan", "Kowalski", 35);
-        System.out.println(p1.toString());
+        Person p2 = new Person("Adam", "Nowak", 32);
+        Person p3 = new Person("Jan", "Nowak", 33);
+        Person p4 = new Person("Adam", "Kowalski", 34);
+        Person p5 = new Person("Janina", "Kowalska", 36);
+
+        ArrayList<Person> lista = new ArrayList<Person>(10);
+        lista.add(p1);
+        lista.add(p2);
+        lista.add(p3);
+        lista.add(p4);
+        lista.add(p5);
+
+        for (Person e: lista) {
+            System.out.println(e.toString());
+        }
 
     }
 }
