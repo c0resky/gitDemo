ef312a7907672a85e75755757b38292f7eb5d272
Piotr <pispiotr92@gmail.com>
2019-12-23 17:27:46 +0100

    Initial commit

---
+++ src/main/java/pack/MainTest.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class MainTest {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Person p1 = new Person("Jan", "Kowalski", 35);
+        System.out.println(p1.toString());
+
+    }
+}
