ea3f3338c0afda5ba8f297625a9c17d20dc4c039
Piotr <pispiotr92@gmail.com>
2020-01-02 21:21:33 +0100

    Ships deployment done

--- src/main/java/pack/BattleshipsMain.java
+++ src/main/java/pack/BattleshipsMain.java
@@ -8,15 +8,15 @@ public class BattleshipsMain {
 
         Scanner scan = new Scanner(System.in);
 
-        String[][] my1 = new String[10][10];
-        String[][] my2 = new String[10][10];
-        String[][] enemy1 = new String[10][10];
-        String[][] enemy2 = new String[10][10];
+        String[][] my1 = new String[12][12];
+        String[][] my2 = new String[12][12];
+        String[][] enemy1 = new String[12][12];
+        String[][] enemy2 = new String[12][12];
         String input;
 
         // usuwamoe nullów  z tablicy
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < 12; i++) {
+            for (int j = 0; j < 12; j++) {
                 my1[i][j] = " ";
                 my2[i][j] = " ";
                 enemy1[i][j] = " ";
@@ -29,22 +29,22 @@ public class BattleshipsMain {
         do {
             // wyświetalnie zawartości tablicy my1 i my2
             System.out.println("  |A |B |C |D |E |F |G |H |I |J         |A |B |C |D |E |F |G |H |I |J");
-            for (int i = 0; i < 10; i++) {
-                if (i < 9) {
-                    System.out.print((i + 1) + " |");
+            for (int i = 1; i < 11; i++) {
+                if (i < 10) {
+                    System.out.print((i) + " |");
                 } else {
-                    System.out.print((i + 1) + "|");
+                    System.out.print((i) + "|");
                 }
-                for (int j = 0; j < 10; j++) {
+                for (int j = 1; j < 11; j++) {
                     System.out.print(my1[i][j] + "  ");
                 }
                 System.out.print("     ");
-                if (i < 9) {
-                    System.out.print((i + 1) + " |");
+                if (i < 10) {
+                    System.out.print((i) + " |");
                 } else {
-                    System.out.print((i + 1) + "|");
+                    System.out.print((i) + "|");
                 }
-                for (int j = 0; j < 10; j++) {
+                for (int j = 1; j < 11; j++) {
                     System.out.print(enemy1[i][j] + "  ");
                 }
 
@@ -59,7 +59,7 @@ public class BattleshipsMain {
                 System.out.println("Incorrect coordinate!");
                 break;
             }
-            my1[X][Y] = "X";
+            my1[X + 1][Y + 1] = "X";
 
         } while (!(input.equals("0")));
     }
--- src/main/java/pack/Enemy.java
+++ src/main/java/pack/Enemy.java
@@ -11,15 +11,24 @@ public class Enemy {
 
     static void placeShips(String[][] table) {
 
-        int gunboats = 0;
-        int destroyers = 5;
+        int gunboats = 4;
+        int destroyers = 3;
         int cruisers = 2;
-        int battleships = 5;
+        int battleships = 1;
+        int a, b, c, d;
 
-        int a = placeGunboats(gunboats, table);
-        int b = placeDestroyers(destroyers, table);
-        int c = placeCruisers(cruisers, table);
-        int d = placeBattleships(battleships, table);
+        do {
+            // czyszczenie tablicy wejsciowej
+            for (int i = 0; i < 12; i++) {
+                for (int j = 0; j < 12; j++) {
+                    table[i][j] = " ";
+                }
+            }
+            a = placeGunboats(gunboats, table);
+            b = placeDestroyers(destroyers, table);
+            c = placeCruisers(cruisers, table);
+            d = placeBattleships(battleships, table);
+        } while (a != gunboats || b != destroyers || c != cruisers || d != battleships);
 
         System.out.println("Enemy has: " + a + " gunboat(s)");
         System.out.println("Enemy has: " + b + " destroyers(s)");
@@ -31,11 +40,19 @@ public class Enemy {
     static int placeGunboats(int howMany, String[][] table) {
         int amount = 0;
         for (int i = 0; i < howMany; i++) {
-            int x = getRandom(10);
-            int y = getRandom(10);
+            int x = getRandom(10) + 1;
+            int y = getRandom(10) + 1;
 
             if (table[x][y] == " ") {
                 table[x][y] = "O";
+                table[x - 1][y] = ".";
+                table[x + 1][y] = ".";
+                table[x][y - 1] = ".";
+                table[x][y + 1] = ".";
+                table[x - 1][y - 1] = ".";
+                table[x - 1][y + 1] = ".";
+                table[x + 1][y - 1] = ".";
+                table[x + 1][y + 1] = ".";
                 amount++;
             }
         }
@@ -45,15 +62,24 @@ public class Enemy {
     static int placeDestroyers(int howMany, String[][] table) {
         int amount = 0;
         for (int i = 0; i < howMany; i++) {
-            int x = getRandom(9);
-            int y = getRandom(9);
+            int x = getRandom(9) + 1;
+            int y = getRandom(9) + 1;
             int rest = getRandom(2);
 
             if (
-                    table[x][y] == " " &&
-                            table[x + rest][y + 1 - rest] == " ") {
+                    table[x][y] == " " && table[x + rest][y + 1 - rest] == " ") {
                 table[x][y] = "O";
                 table[x + rest][y + 1 - rest] = "O";
+                table[x - rest][y - 1 + rest] = "."; // po lewej lub powyżej
+                table[x + 1 - rest][y + rest] = "."; // prawa lub dół
+                table[x - 1 + rest][y - rest] = "."; // lewa lub góra
+                table[x + 2 * rest][y - 2 * rest + 2] = "."; // koniec
+                table[x + 1][y + 1] = "."; // prawy dół
+                table[x + 1][y - 1] = ".";  // lewy dół
+                table[x - 1][y + 1] = "."; // prawa góra
+                table[x - 1][y - 1] = "."; // lewa góra
+                table[x + rest + 1][y - rest + 2] = "."; // prawy dół koniec
+                table[x + 3 * rest - 1][y - 3 * rest + 2] = "."; // lewa lub góra koniec
                 amount++;
             }
         }
@@ -63,17 +89,30 @@ public class Enemy {
     static int placeCruisers(int howMany, String[][] table) {
         int amount = 0;
         for (int i = 0; i < howMany; i++) {
-            int x = getRandom(8);
-            int y = getRandom(8);
+            int x = getRandom(8) + 1;
+            int y = getRandom(8) + 1;
             int rest = getRandom(2);
 
             if (
                     table[x][y] == " " &&
                             table[x + rest][y + 1 - rest] == " " &&
-                            table[x + rest * 2][y + (1 - rest) * 2] == " ") {
+                            table[x + rest * 2][y + (1 - rest) * 2] == " "
+            ) {
                 table[x][y] = "O";
                 table[x + rest][y + 1 - rest] = "O";
                 table[x + rest * 2][y + (1 - rest) * 2] = "O";
+                table[x - rest][y - 1 + rest] = "."; // po lewej lub powyżej
+                table[x + 1 - rest][y + rest] = "."; // prawa lub dół
+                table[x - 1 + rest][y - rest] = "."; // lewa lub góra
+                table[x + 3 * rest][y - 3 * rest + 3] = "."; // koniec
+                table[x + 1][y + 1] = "."; // prawy dół
+                table[x + 1][y - 1] = ".";  // lewy dół
+                table[x - 1][y + 1] = "."; // prawa góra
+                table[x - 1][y - 1] = "."; // lewa góra
+                table[x + rest + 1][y - rest + 2] = "."; // prawy dół 3
+                table[x + 3 * rest - 1][y - 3 * rest + 2] = "."; // lewa lub góra 3
+                table[x + 2 * rest + 1][y - 2 * rest + 3] = "."; // prawy dół koniec
+                table[x + 4 * rest - 1][y - 4 * rest + 3] = "."; // lewa lub góra koniec
                 amount++;
             }
         }
@@ -83,8 +122,8 @@ public class Enemy {
     static int placeBattleships(int howMany, String[][] table) {
         int amount = 0;
         for (int i = 0; i < howMany; i++) {
-            int x = getRandom(7);
-            int y = getRandom(7);
+            int x = getRandom(7) + 1;
+            int y = getRandom(7) + 1;
             int rest = getRandom(2);
 
             if (
@@ -96,6 +135,20 @@ public class Enemy {
                 table[x + rest][y + 1 - rest] = "O";
                 table[x + rest * 2][y + (1 - rest) * 2] = "O";
                 table[x + rest * 3][y + (1 - rest) * 3] = "O";
+                table[x - rest][y - 1 + rest] = "."; // po lewej lub powyżej
+                table[x + 1 - rest][y + rest] = "."; // prawa lub dół
+                table[x - 1 + rest][y - rest] = "."; // lewa lub góra
+                table[x + 4 * rest][y - 4 * rest + 4] = "."; // koniec
+                table[x + 1][y + 1] = "."; // prawy dół
+                table[x + 1][y - 1] = ".";  // lewy dół
+                table[x - 1][y + 1] = "."; // prawa góra
+                table[x - 1][y - 1] = "."; // lewa góra
+                table[x + rest + 1][y - rest + 2] = "."; // prawy dół 3
+                table[x + 3 * rest - 1][y - 3 * rest + 2] = "."; // lewa lub góra 3
+                table[x + 2 * rest + 1][y - 2 * rest + 3] = "."; // prawy dół 4
+                table[x + 4 * rest - 1][y - 4 * rest + 3] = "."; // lewa lub góra 4
+                table[x + 3 * rest + 1][y - 3 * rest + 4] = "."; // prawy dół koniec
+                table[x + 5 * rest - 1][y - 5 * rest + 4] = "."; // lewa lub góra koniec
                 amount++;
             }
         }
